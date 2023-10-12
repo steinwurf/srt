a4510c45704f660f03d1174c0c01090802336cb1
Mikkel Højlund Larsen <mikkelhojlundgrontmij@gmail.com>
2023-10-12 15:25:49 +0200

    no waf unpacked

--- .waf3-2.0.22-e6b44ba14c1947e7b6e49f0d693672c2/waflib/extras/wurf/git_url.py
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/env python
-# encoding: utf-8
-# WARNING! Do not edit! https://waf.io/book/index.html#_obtaining_the_waf_file
-
-import collections
-GitUrl=collections.namedtuple("GitUrl","protocol host path")
--- .waf3-2.0.22-e6b44ba14c1947e7b6e49f0d693672c2/waflib/extras/wurf/waf_build_context.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env python
-# encoding: utf-8
-# WARNING! Do not edit! https://waf.io/book/index.html#_obtaining_the_waf_file
-
-from waflib.Build import BuildContext
-class WafBuildContext(BuildContext):
-	def execute_build(self):
-		self.recurse_dependencies()
-		super(WafBuildContext,self).execute_build()
