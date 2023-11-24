25bb6d47c9d29ac589e729675fd6dbc00bc56912
suhrm <suhrminator@gmail.com>
2023-11-24 05:58:21 -0800

    Updated cmake for windows wip

--- test/src/test_srt.cpp
+++ test/src/test_srt.cpp
@@ -1,10 +1,14 @@
 
+#include <cassert>
 #include <gtest/gtest.h>
+#include <iostream>
 #include <srt/srt.h>
 #include <srt/udt.h>
 #include <srt/version.h>
 
 TEST(test_srt, lib) {
-  srt_startup();
-  srt_cleanup();
+  int ret = srt_startup();
+  assert(ret == SRT_SUCCESS);
+  ret = srt_cleanup();
+  assert(ret == SRT_SUCCESS);
 }
