6c52e7a2e03945cfc9d129fe3e51bd9b20bee899
suhrm <suhrminator@gmail.com>
2023-11-27 03:51:18 -0800

    Updated cmake compiles on windows now

--- test/src/test_srt.cpp
+++ test/src/test_srt.cpp
@@ -5,6 +5,16 @@
 #include <srt/srt.h>
 #include <srt/udt.h>
 #include <srt/version.h>
+#include <platform/config.hpp>
+
+
+#if defined(PLATFORM_WINDOWS)
+#include <Winsock2.h>
+#define _WINSOCKAPI_
+#include <io.h>
+#include <ws2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
+#endif
 
 TEST(test_srt, lib) {
   int ret = srt_startup();
