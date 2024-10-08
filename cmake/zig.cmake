a5a2c67771eadf776333d127e920943915f36d4c
Mikkel Højlund Larsen <mikkelhojlundgrontmij@gmail.com>
2024-10-08 14:16:10 +0200

    remove cmake folder

--- cmake/zig.cmake
+++
@@ -1,6 +0,0 @@
-
-
-set(CMAKE_C_COMPILER "zig" cc -target ${ZIG_TARGET})
-set(CMAKE_CXX_COMPILER "zig" c++ -target ${ZIG_TARGET})
-set(CMAKE_AR "${CMAKE_CURRENT_LIST_DIR}/zig-ar.sh")
-set(CMAKE_RANLIB "${CMAKE_CURRENT_LIST_DIR}/zig-ranlib.sh")
